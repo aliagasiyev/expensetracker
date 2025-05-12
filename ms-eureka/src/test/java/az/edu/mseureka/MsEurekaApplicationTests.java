e593fec9c7d9ce2958cf595883a1e307f4524e0c
aliagasiyev <agasiyevali2@gmail.com>
2025-05-13 01:08:33 +0400

    initial Eureka Server setup and configuration

---
+++ ms-eureka/src/test/java/az/edu/mseureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.mseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
