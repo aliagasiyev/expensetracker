50aae94199e14057a1da7227dc98855295743fd8
aliagasiyev <agasiyevali2@gmail.com>
2025-05-26 00:04:30 +0400

    Fix JwtAuthenticationToken constructor - remove setAuthenticated(true) call

--- ms-auth/src/main/java/az/edu/msauth/security/JwtAuthenticationToken.java
+++ ms-auth/src/main/java/az/edu/msauth/security/JwtAuthenticationToken.java
@@ -13,7 +13,7 @@ public class JwtAuthenticationToken extends UsernamePasswordAuthenticationToken 
     public JwtAuthenticationToken(UserDetails principal, Collection<? extends GrantedAuthority> authorities, Long userId) {
         super(principal, null, authorities);
         this.userId = userId;
-        setAuthenticated(true);
+        // Don't call setAuthenticated(true) - the parent constructor already handles this
     }
     
     public Long getUserId() {
