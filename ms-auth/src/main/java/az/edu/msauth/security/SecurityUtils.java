153c1fea343a7c66868bb2276e95d422ffd28de1
aliagasiyev <agasiyevali2@gmail.com>
2025-06-01 12:22:42 +0400

    fix(gateway): proper JWT delete by id profile and public endpoint configuration

--- ms-auth/src/main/java/az/edu/msauth/security/SecurityUtils.java
+++ ms-auth/src/main/java/az/edu/msauth/security/SecurityUtils.java
@@ -2,10 +2,7 @@ package az.edu.msauth.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import az.edu.msauth.entity.User;
-import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
